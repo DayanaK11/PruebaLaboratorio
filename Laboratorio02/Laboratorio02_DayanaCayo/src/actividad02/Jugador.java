9a3fde5963ac49863ff45a723db0bedd7e7f875d
DayanaK11 <dayanacayol@gmail.com>
2024-05-15 16:41:20 -0500

    añadiendo lab02

---
+++ Laboratorio02/Laboratorio02_DayanaCayo/src/actividad02/Jugador.java
@@ -0,0 +1,22 @@
+//Laboratorio Nro 2 - Ejercicio2
+//Autor: Dayana Katherine Cayo Lahuana 
+package actividad02;
+
+public class Jugador {
+	private String nombre;
+	private int nivel;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNivel() {
+		return nivel;
+	}
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+}
